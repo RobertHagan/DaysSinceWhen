0b3a0b4bf414fed5d57d548169a8aafa92be690c
Robert Hagan <RobertHagan@gmail.com>
2017-08-23 22:34:40 +0430

    DaysSinceWhen first cmmmit on a local machine.

---
+++ R/DaysSinceWhen.R
@@ -0,0 +1,16 @@
+#' DaysSinceWhen
+#'
+#' Computes the number of days that have passed since
+#'     the date entered.
+#'
+#' @param dob A character string in the form "yyyy-mm-dd"
+#'
+#' @return a numeric decimal numeric value
+#' @export
+#'
+#' @examples DaysOld <- DaysSinceWhen("1948-6-6")
+#' @seealso \code{\link{Sys.time}}
+DaysSinceWhen <- function(dob) {
+  DaysOld <- as.numeric(difftime(Sys.time(),dob, units = "days"))
+  return(DaysOld)
+}
