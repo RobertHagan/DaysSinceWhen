9b92f65307e8bc042cdc6fe26f16ab79732e31a8
Robert Hagan <RobertHagan@gmail.com>
2017-08-24 17:40:27 +0430

    working testthat

---
+++ tests/testthat.R
@@ -0,0 +1,4 @@
+library(testthat)
+library(DaysSinceWhen)
+
+test_check("DaysSinceWhen")
--- tests/testthat/test.R
+++ tests/testthat/test.R
@@ -1,8 +1,8 @@
 library(testthat)
 library(DaysSinceWhen)
 
-test_check("DaysSinceWhen")
-test_that("consistend computation of days",{
+#test_check("DaysSinceWhen")
+test_that("consistant computation of days",{
   expect_equal(as.integer(DaysSinceWhen("2000-1-1")),
                as.integer(DaysSinceWhen("2000-1-1")))
 })
